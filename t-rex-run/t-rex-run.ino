f24faf4f967fab0213470e46ea9d763cbc692801
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-04-11 18:18:34 -0400

    Intital commit.

---
+++ t-rex-run/t-rex-run.ino
@@ -0,0 +1,14 @@
+/**
+ * Used to play Google's T-Rex run game with no machine learning just servos and LDR sensors. 
+ * @author Jeffrey Boisvert
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
