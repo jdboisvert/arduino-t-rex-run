ed706d752e16bbff324a70a4e0cb6884fffec6d9
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-04-11 20:29:25 -0400

    Added basic logic to reset, jump and duck

--- t-rex-run/t-rex-run.ino
+++ t-rex-run/t-rex-run.ino
@@ -14,7 +14,7 @@ const int DUCK_SERVO_PIN = 4;
 
 //Servo positions
 const int JUMP_OFF_POSITION = 10; //Change this based on how your servos are set up 
-const int JUMP_ON_POSITION = 25; //Change this based on how your servos are set up 
+const int JUMP_ON_POSITION = 30; //Change this based on how your servos are set up 
 const int DUCK_OFF_POSITION = 105; //Change this based on how your servos are set up 
 const int DUCK_ON_POSITION = 120; //Change this based on how your servos are set up 
 
@@ -26,24 +26,34 @@ void setup() {
 }
 
 void loop() {
-  offPosition();     
+  jump();   
   delay(180); 
-  onPosition();
-  delay(180);  
-  offPosition();          
+  duck();
+  delay(180);      
 }
 
+/**
+ * Put both keys off the trigger
+ */
 void startPosition(){
   jumpServo.write(JUMP_OFF_POSITION); 
   duckServo.write(DUCK_OFF_POSITION); 
 }
 
-void offPosition(){
+/**
+ * Used to press the key to jump
+ */
+void jump(){
+  jumpServo.write(JUMP_ON_POSITION); 
+  delay(360); 
   jumpServo.write(JUMP_OFF_POSITION); 
-  duckServo.write(DUCK_OFF_POSITION); 
 }
 
-void onPosition(){
-  jumpServo.write(JUMP_ON_POSITION); 
+/**
+ * Used to press the key to duck 
+ */
+void duck(){
   duckServo.write(DUCK_ON_POSITION); 
+  delay(360); 
+  duckServo.write(DUCK_OFF_POSITION); 
 }
